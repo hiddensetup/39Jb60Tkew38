d84a38070c43ce34a9407ede653e562068f67bbc
hiddensetup <franka@dublin.com>
2024-08-05 03:22:41 -0300

    done-2

--- public/js/app.js
+++ public/js/app.js
@@ -153,13 +153,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, (error) => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-    });
-  }
+  
   
